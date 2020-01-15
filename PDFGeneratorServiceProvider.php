d15aa0dcb8f3a09de03477afd13d2246ed0c0f6c
Chris <chris@builtbypixel.com>
2020-01-15 10:37:46 +0000

    quick update

--- PDFGeneratorServiceProvider.php
+++ PDFGeneratorServiceProvider.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * This file is part of consoletvs/invoices.
- *
- * (c) Chris Mills <chris@byte-digital.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace ByteDigital\PDFGenerator;
 
